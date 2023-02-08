e69e1dd97d01f5a4bbae6a219f4dae093d1a901d
gunstillrattles <marlen.mukashev@narxoz.kz>
2023-02-09 01:20:37 +0600

    Добавьте файлы проекта.

---
+++ ConsoleApplication18/ConsoleApplication18.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <string>
+#include <cstdlib>
+using namespace std;
+int main()
+{
+	string text = "ATTACKATDAWN";
+	string key = "LEMON";
+	int keyLen = key.length();
+	for (int i = key.length(); i < text.length(); i++) {
+		key += ' ';
+
+	}
+	int numOfCycles = 0;
+	for (int i = keyLen; i < text.length(); i++) {
+		for (int j = numOfCycles; j < text.length(); j++) {
+			key[i] = key[j];
+			numOfCycles++;
+			break;
+		}
+	}
+	string encodeResult = key;
+	string decodeResult = key;
+	cout << key << endl;
+	for (int i = 0; i < text.length(); i++) {
+		encodeResult[i] = ((text[i] + key[i]) % 26) + 65;
+	}
+	int temp;
+	for (int i = 0; i < text.length(); i++) {
+		temp = encodeResult[i] - key[i];
+		if (temp < 0) {
+			temp = (temp * 1) + 26;
+		}
+		decodeResult[i] = temp % 26 + 65;
+	}
+	cout << encodeResult << endl;
+	cout << decodeResult << endl;
+}
